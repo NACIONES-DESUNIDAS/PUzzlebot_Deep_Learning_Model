a854461ee58791b5b837a734e1e42640c0d0288e
Luis Dario <dariophoskyhinojosa@gmail.com>
2022-06-13 09:30:55 -0500

    logica filtrado de banderas

--- pruebas/segmentation.py
+++ pruebas/segmentation.py
@@ -1,77 +1,71 @@
 # programa para captar video en opencv
 
 import cv2 as cv2
+from tensorflow.keras.models import load_model
 
-capture = cv2.VideoCapture("puzzlebot_record2.avi")
+
+capture = cv2.VideoCapture("no_speed.avi")
+
+model = load_model("puzzlebot_model.h5")
+model.summary()
 import numpy as np
 
-def preprocessImage(img):
-    # Method used to preprocess the node input image, the image processing must be divided in:
-    # 1 - Resize the input image to a specified image scale factor.
-    # 2 - Rotate the image if required.
-    # 3 - Apply an adequate Gaussian Blur to the image, modify the filter kernel as required.
-    # 4 - Return the final processed image
 
-    ##########################################################################################################
-    # TODO: Complete the class method definition using the previous description
-    ##########################################################################################################
+IMG_SHAPE = (150,150,1)
+IMG_TUPPLE_SHAPE = (150,150)
+BATCH_SIZE= 128
+dims = 150
 
-    # Your code here...
-    width = int(img.shape[0]*50/100)
-    height = int(img.shape[1]*50/100)
-    img = cv2.resize(img,(height,width))
-    img = cv2.rotate(img,cv2.ROTATE_180)
-    resized = img.copy()
-    #img =cv2.GaussianBlur(img,(7,7),0)
-    ##########################################################################################################
-    return img,resized
+def imageProcessing(image):
+    image = image.astype(np.uint8)
+    gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+    gray = cv2.equalizeHist(gray)
+    gray = cv2.resize(gray,IMG_TUPPLE_SHAPE)
+    gray = gray.reshape(1,dims,dims,1)
+    return gray.astype(np.float64)/255
+
+
+def getCalssName(classNo):
+    labels = {0: 'stop_signal', 1: 'aplastame', 2: 'right_signal', 3: 'left_signal', 4: 'up_signal', 5: 'around_signal'}
+    return labels[classNo]
+
+
+
 
 
 def getHoughCircles(img):
     gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 
-    circles_img = cv2.HoughCircles(gray,cv2.HOUGH_GRADIENT,1,100,param1=50,param2=30,minRadius=15,maxRadius=30)
+    circles_img = cv2.HoughCircles(gray,cv2.HOUGH_GRADIENT,1,100,param1=50,param2=30,minRadius=20,maxRadius=30)
     #circles_img = np.uint16(np.around(circles_img))
     return circles_img
-    """
-    for i in circles_img[0,:]:
-        cv2.circle(img,(i[0],i[1]),i[2],(0,255,0),2)
-        cv2.circle(img,(i[0],i[1]),2,(0,0,255),3)
-    return img, circles_img
-    """
 
 
+# Logica de loslabelsque se mandan a nagegacion
 
 
-
-def equalizeSV(hsv):
-    H,S,V = cv2.split(hsv)
-    S = cv2.equalizeHist(S)
-    V = cv2.equalizeHist(S)
-    equalized = cv2.merge([H,S,V])
-    return equalized
-
+class SignalDetector:
+    def __init__(self):
+        # classatributes
+        self.lastLabel = None
+        self.labelCounter = 0
+        self.labelThreshold = 4
 
 
+    def pubLabelFlag(self,label):
+        if self.lastLabel == label:
+            self.labelCounter += 1
+        if self.labelCounter > self.labelThreshold:
+            self.labelCounter = 0
+            flag = True if self.lastLabel != "not_found" else False
+            print(self.lastLabel,flag)
 
-def extractWhitePixels(img):
-
-    #upperWhite = np.array([0,50,255])
-    #lowerWhite = np.array([0,0,0])
-    upperWhite = np.array([180,255,255])
-    lowerWhite = np.array([50,0,0])         
-    hsv = cv2.cvtColor(img.copy(),cv2.COLOR_BGR2RGB)
-    hsv = equalizeSV(hsv)
-
-    filtered = cv2.inRange(hsv,lowerb=lowerWhite,upperb=upperWhite)
-    masked = cv2.bitwise_and(img,img,mask=filtered)
+        self.lastLabel = label
 
 
-    
-    return masked
+def getSegementedImages(circles,img,d):
 
 
-def getSegementedImages(circles,img):
     images = list()
     outputImg = img.copy()
     if circles is not None:
@@ -84,13 +78,29 @@ def getSegementedImages(circles,img):
             bottomRightCorner =  (x+(rad),y+(rad))
             outputImg = cv2.rectangle(outputImg,topLeftCorner,bottomRightCorner,(255,0,0),5)
             segemented = img[y-rad:y+rad,x-rad:x+rad,:]
-            #print(segemented.shape)
             try:
-                cv2.imshow("video",segemented)
+            
+                image = segemented.astype(np.uint8)
+                gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+                gray = cv2.equalizeHist(gray)
+                gray = cv2.resize(gray,IMG_TUPPLE_SHAPE)
+                cv2.imshow("video",gray)
+                gray = gray.reshape(1,150,150,1)
+                gray = gray.astype(np.float64)/255
+                prediction = model.predict(gray)
+                index = np.argmax(prediction)
+                prob = prediction[0][index]
+                label = getCalssName(index) if prob > 0.9 else "not_found"
+                #d.pubLabelFlag(label)
+                print(label)
             except:
                 continue
+            #cv2.imshow("video",outputImg)
 
     return outputImg
+
+
+
 
 
 
@@ -99,9 +109,14 @@ dims = 3
 d = 1
 e = 5
 
-while True: # process videos frame by frame
-    isTrue, img = capture.read()
 
+d = SignalDetector()
+
+
+while True: # process videos frame by frame
+
+    isTrue, img = capture.read()
+    # private atributes
     # preprocesamiento
     width = int(img.shape[0]*50/100)
     height = int(img.shape[1]*50/100)
@@ -109,7 +124,7 @@ while True: # process videos frame by frame
     img = cv2.rotate(img,cv2.ROTATE_180)
 
     circles = getHoughCircles(img)
-    images = getSegementedImages(circles=circles,img=img)
+    images = getSegementedImages(circles=circles,img=img,d=d)
     #cv2.imshow("video",images)
 
 
